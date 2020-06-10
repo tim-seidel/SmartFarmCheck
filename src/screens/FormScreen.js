9aaf6ac68a26bee64148e06533ee4cb24ede00ea
Tim Seidel <account@timseidel.de>
2020-06-10 12:01:40 +0200

    Transformed FormScreen to functional component to allow easier managment of the paging state.

--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, VirtualizedList, Alert, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { HeaderButtons, HeaderButton, Item } from 'react-navigation-header-buttons';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useNavigation } from '@react-navigation/native';
 
 import NoContentView from "../components/NoContentView";
 import QuestionView from "../components/QuestionView";
@@ -13,26 +12,20 @@ const SFCHeaderButton = props => (
     <HeaderButton {...props} IconComponent={Icon} iconSize={24} color={Colors.white} />
 )
 
-class FormScreen extends React.Component {
+const FormScreen = props => {
+    const [mode, setMode] = useState('list')
+    const [questionState, setQuestionState] = useState({ isLoaded: false, error: null, questions: [] })
+    const [pagingIndex, setPagingIndex] = useState(0)
+    const [formId, setFormId] = useState(0)
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            mode: 'list',
-            questionIndex: 0,
-            questions: [],
-            formId: 0
+    useEffect(() => {
+        if (!questionState.isLoaded) {
+            loadQuestions();
         }
-    }
+    }, [questionState.isLoaded])
 
-    componentDidMount() {
-        this.loadQuestions();
-    }
-
-    loadQuestions() {
-        if (!this.state.isLoaded) {
+    function loadQuestions() {
+        if (!questionState.isLoaded) {
             fetch('https://pas.coala.digital/v1/questions', {
                 headers: {
                     'Content-Type': 'application/json',
@@ -41,184 +34,162 @@ class FormScreen extends React.Component {
             })
                 .then(response => response.json())
                 .then(json => {
-                    this.setState({
-                        isLoaded: true,
-                        questions: json,
-                        error: null,
-                        formId: 0
-                    })
+                    setQuestionState({ isLoaded: true, error: null, questions: json })
                 })
                 .catch(error => {
                     console.log("error", error)
-                    this.setState({
-                        isLoaded: false,
-                        error: error
-                    })
+                    setQuestionState(qs => ({ isLoaded: false, error: error, questions: qs.questions }))
                 })
         }
     }
 
-    onRetryHandler() {
-        this.setState({
-            isLoaded: false,
-            error: null
-        }, this.loadQuestions.bind(this));
+    function retryHandler() {
+        setLoadingState({ isLoaded: false, error: null })
     }
 
-    inputChangeHandler(question, input, validity) {
+    function inputChangeHandler(question, input, validity) {
         question.input = input
         question.validity = validity
     }
 
-    layoutChangeHandler() {
-        this.setState(prevState => ({
-            mode: prevState.mode === 'list' ? 'single' : 'list'
-        }))
+    function layoutChangeHandler() {
+        setMode(mode => mode === 'list' ? 'single' : 'list')
     }
 
-    questionPagingHandler(toNext) {
-        const qi = this.state.questionIndex;
-        var qNext = toNext ? qi + 1 : qi - 1;
+    function questionPagingHandler(toNext) {
+        var qNext = toNext ? pagingIndex + 1 : pagingIndex - 1;
         if (qNext < 0) {
             qNext = 0
         }
-        if (qNext > (this.state.questions.length - 1)) {
-            qNext = this.state.questions.length - 1
+
+        const max = questionState.questions.length - 1
+        if (qNext > max) {
+            qNext = max
         }
-        this.setState({
-            questionIndex: qNext
-        })
+        setPagingIndex(qNext)
     }
+    props.navigation.setOptions({
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={SFCHeaderButton}>
+                <Item iconName="format-list-checkbox" title="Layout" onPress={layoutChangeHandler} />
+            </HeaderButtons>
+        )
+    })
 
-    render() {
-        const { error, isLoaded, questions, formId, mode, questionIndex } = this.state;
-        console.log("FormScreen.render()")
+    const { isLoaded, error, questions } = questionState;
+    console.log("FormScreen.render()", isLoaded, error, questions.length)
+    if (error) {
+        return <NoContentView icon="emoticon-sad-outline" onRetry={retryHandler} title="Aktuell kann das Formular nicht geladen werden. Bitte überprüfen Sie Ihre Internetverbindung oder versuchen Sie es später erneut."></NoContentView>
+    } else if (!isLoaded) {
+        return <NoContentView icon="cloud-download" loading title="Laden des akutellsten Fragebogens..."></NoContentView>
+    } else if (questions.length === 0) {
+        return <NoContentView icon="emoticon-sad-outline" onRetry={retryHandler} title="Aktuell können die Fragen des Fragebogens nicht geladen werden. Bitte überprüfen Sie Ihre Internetverbindung oder versuchen Sie es später erneut."></NoContentView>
+    } else {
+        var questionContent = null;
+        if (mode === 'list') {
+            questionContent = (
+                <View style={styles.listContainer}>
+                    <VirtualizedList
+                        contentContainerStyle={styles.listContent}
+                        data={questions}
+                        renderItem={({ item, index }) => <QuestionView formId={formId} onInputChanged={(input, validity) => inputChangeHandler(item, input, validity)} index={index + 1} question={item} key={item.uuid} />}
+                        keyExtractor={item => item.uuid}
+                        getItemCount={() => questions.length}
+                        getItem={(data, index) => { return questions[index] }}
+                    />
+                </View>)
+        } else if (mode === 'single') {
+            const currentQuestion = questions[pagingIndex]
+            const canNavigatePrevious = pagingIndex > 0
+            const canNavigateNext = (pagingIndex < (questions.length - 1))
 
-        this.props.navigation.setOptions({
-            headerRight: () => (
-                <HeaderButtons HeaderButtonComponent={SFCHeaderButton}>
-                    <Item iconName="format-list-checkbox" title="Layout" onPress={() => this.layoutChangeHandler()} />
-                </HeaderButtons>
-            )
-        })
-
-        if (error) {
-            return <NoContentView icon="emoticon-sad-outline" onRetry={this.onRetryHandler.bind(this)} title="Aktuell kann das Formular nicht geladen werden. Bitte überprüfen Sie Ihre Internetverbindung oder versuchen Sie es später erneut."></NoContentView>
-        } else if (!isLoaded) {
-            return <NoContentView icon="cloud-download" loading title="Laden des akutellsten Fragebogens..."></NoContentView>
-        } else if (questions.length === 0) {
-            return <NoContentView icon="emoticon-sad-outline" onRetry={this.onRetryHandler.bind(this)} title="Aktuell können die Fragen des Fragebogens nicht geladen werden. Bitte überprüfen Sie Ihre Internetverbindung oder versuchen Sie es später erneut."></NoContentView>
-        } else {
-            var questionContent = null;
-            if (mode === 'list') {
-                questionContent = (
-                    <View style={styles.listContainer}>
-                        <VirtualizedList
-                            contentContainerStyle={styles.listContent}
-                            data={questions}
-                            renderItem={({ item, index }) => <QuestionView formId={formId} onInputChanged={(input, validity) => this.inputChangeHandler(item, input, validity)} index={index + 1} question={item} key={item.uuid} />}
-                            keyExtractor={item => item.uuid}
-                            getItemCount={() => questions.length}
-                            getItem={(data, index) => { return questions[index] }}
-                        />
-                    </View>)
-            } else if (mode === 'single') {
-                const currentQuestion = questions[questionIndex]
-                const canNavigatePrevious = questionIndex > 0
-                const canNavigateNext = (questionIndex < (questions.length - 1))
-
-                questionContent = (
-                    <View style={styles.singleQuestionLayoutContainer}>
-                        <QuestionView formId={formId} onInputChanged={(input, validity) => this.inputChangeHandler(currentQuestion, input, validity)} index={questionIndex + 1} question={currentQuestion} />
-                        <View style={styles.questionPagingRow}>
-                            <TouchableOpacity activeOpacity={0.7} disabled={!canNavigatePrevious} onPress={() => { this.questionPagingHandler(false) }} style={canNavigatePrevious ? styles.pagingButton : styles.pagingButtonDisabled}>
-                                <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "flex-start" }}>
-                                    <Icon
-                                        name="chevron-left"
-                                        size={24}
-                                        color={Colors.white}>
-                                    </Icon>
-                                    <Text
-                                        numberOfLines={1}
-                                        lineBreakMode="tail"
-                                        ellipsizeMode="tail"
-                                        style={{ fontSize: 16, color: Colors.white }}>
-                                        Zurück
+            questionContent = (
+                <View style={styles.singleQuestionLayoutContainer}>
+                    <QuestionView formId={formId} onInputChanged={(input, validity) => inputChangeHandler(currentQuestion, input, validity)} index={pagingIndex + 1} question={currentQuestion} />
+                    <View style={styles.questionPagingRow}>
+                        <TouchableOpacity activeOpacity={0.7} disabled={!canNavigatePrevious} onPress={() => { questionPagingHandler(false) }} style={canNavigatePrevious ? styles.pagingButton : styles.pagingButtonDisabled}>
+                            <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "flex-start" }}>
+                                <Icon
+                                    name="chevron-left"
+                                    size={24}
+                                    color={Colors.white}>
+                                </Icon>
+                                <Text
+                                    numberOfLines={1}
+                                    lineBreakMode="tail"
+                                    ellipsizeMode="tail"
+                                    style={{ fontSize: 16, color: Colors.white }}>
+                                    Zurück
                                     </Text>
-                                </View>
-                            </TouchableOpacity>
-                            <Text style={styles.pageInfo}>{questionIndex + 1}/{questions.length}</Text>
-                            <TouchableOpacity disabled={!canNavigateNext} activeOpacity={0.7} onPress={() => { this.questionPagingHandler(true) }} style={canNavigateNext ? styles.pagingButton : styles.pagingButtonDisabled}>
-                                <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "flex-end" }}>
-                                    <Text
-                                        numberOfLines={1}
-                                        lineBreakMode="tail"
-                                        ellipsizeMode="tail"
-                                        style={{ fontSize: 16, color: Colors.white }}>
-                                        Weiter
+                            </View>
+                        </TouchableOpacity>
+                        <Text style={styles.pageInfo}>{pagingIndex + 1}/{questions.length}</Text>
+                        <TouchableOpacity disabled={!canNavigateNext} activeOpacity={0.7} onPress={() => { questionPagingHandler(true) }} style={canNavigateNext ? styles.pagingButton : styles.pagingButtonDisabled}>
+                            <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "flex-end" }}>
+                                <Text
+                                    numberOfLines={1}
+                                    lineBreakMode="tail"
+                                    ellipsizeMode="tail"
+                                    style={{ fontSize: 16, color: Colors.white }}>
+                                    Weiter
                                     </Text>
-                                    <Icon
-                                        name="chevron-right"
-                                        size={24}
-                                        color={Colors.white}>
-                                    </Icon>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                )
-            }
-            return (
-                <View style={styles.container}>
-                    {questionContent}
-                    <View style={mode === 'single' ? styles.optionsRowHalf : styles.optionsRow}>
-                        <View style={styles.wrapperLeft}>
-                            <IconButton outlined icon="close" text="Zurücksetzen" onPress={() => { this.onResetHandler() }} ></IconButton>
-                        </View>
-                        <View style={styles.wrapperRight}>
-                            <IconButton icon="chart-areaspline" text="Jetzt berechnen" onPress={() => this.onCalculateHandler()}  ></IconButton>
-                        </View>
+                                <Icon
+                                    name="chevron-right"
+                                    size={24}
+                                    color={Colors.white}>
+                                </Icon>
+                            </View>
+                        </TouchableOpacity>
                     </View>
                 </View>
             )
         }
+        return (
+            <View style={styles.container}>
+                {questionContent}
+                <View style={mode === 'single' ? styles.optionsRowHalf : styles.optionsRow}>
+                    <View style={styles.wrapperLeft}>
+                        <IconButton outlined icon="close" text="Zurücksetzen" onPress={resetHandler} ></IconButton>
+                    </View>
+                    <View style={styles.wrapperRight}>
+                        <IconButton icon="chart-areaspline" text="Jetzt berechnen" onPress={calculateHandler}  ></IconButton>
+                    </View>
+                </View>
+            </View>
+        )
     }
 
-    onResetHandler() {
+    function resetHandler() {
         Alert.alert('Wirklich zurücksetzten?', 'Wenn Sie das Formular zurücksetzten werden Ihre bisherigen Eingaben gelöscht. Möchten Sie dies?', [
-            { text: "Zurücksetzen", onPress: () => this.resetForm(), style: "destructive" },
+            { text: "Zurücksetzen", onPress: () => resetForm(), style: "destructive" },
             { text: "Abbrechen", style: "default" },
         ],
             { cancelable: false });
         return;
     }
 
-    resetForm() {
-        console.log("FormID", this.state.formId)
-        this.setState(
-            { formId: (this.state.formId + 1) }
-        )
+    function resetForm() {
+        setFormId(formId + 1)
     }
 
-    onCalculateHandler() {
-        const questions = this.state.questions;
-        const indicieserror = []
+    function calculateHandler() {
+        const indiciesError = []
+        const indiciesEmpty = []
 
         questions.forEach((q, index) => {
             if (q.validity === 'invalid') {
-                indicieserror.push(index + 1)
+                indiciesError.push(index + 1)
             }
         });
 
-        if (indicieserror.length > 0) {
-            Alert.alert('Fehlerhafte Eingaben', 'Bitte berichtigen Sie zuerst die ungültigen Eingaben, bevor Sie das Formualar absenden. Fehlerhaft: (' + indicieserror.join(', ') + ')', [
+        if (indiciesError.length > 0) {
+            Alert.alert('Fehlerhafte Eingaben', 'Bitte berichtigen Sie zuerst die ungültigen Eingaben, bevor Sie das Formualar absenden. Fehlerhaft: (' + indiciesError.join(', ') + ')', [
                 { text: "Okay", onPress: () => console.log("Canceled sending"), style: "cancel" },
             ],
                 { cancelable: false });
             return;
         } else {
 
-            const indiciesEmpty = []
             questions.forEach((q, index) => {
                 if (!q.input) {
                     indiciesEmpty.push(index + 1)
@@ -227,16 +198,16 @@ class FormScreen extends React.Component {
             if (indiciesEmpty.length > 0) {
                 Alert.alert('Formular absenden?', 'Sie haben noch nicht alle Fragen beantwortet (' + indiciesEmpty.join(', ') + '). Möchten Sie das Formular dennoch absenden?', [
                     { text: "Abbrechen", onPress: () => console.log("Canceled sending"), style: "cancel" },
-                    { text: "Absenden", onPress: () => this.gotoEvaluation(questions), style: "default" }
+                    { text: "Absenden", onPress: () => gotoEvaluation(questions), style: "default" }
                 ],
                     { cancelable: false });
             } else {
-                this.gotoEvaluation(questions);
+                gotoEvaluation(questions);
             }
         }
     }
 
-    gotoEvaluation(questions) {
+    function gotoEvaluation(questions) {
         const send = []
         questions.forEach(q => {
             if (q.input) {
@@ -244,7 +215,7 @@ class FormScreen extends React.Component {
             }
         })
         const data = JSON.stringify(send);
-        this.props.navigation.navigate("Evaluation", data)
+        props.navigation.navigate("Evaluation", data)
 
     }
 }
@@ -321,8 +292,4 @@ styles = StyleSheet.create({
 });
 
 // Wrap for navigation
-export default function (props) {
-    const navigation = useNavigation();
-
-    return <FormScreen {...props} navigation={navigation} />;
-}
+export default FormScreen
