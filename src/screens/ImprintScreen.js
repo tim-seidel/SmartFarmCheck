7a2c685109c21b336d406417ec0ad9451a352567
Tim Seidel <timseidel@83ad3357.funky.hs-osnabrueck.de>
2020-07-08 11:40:29 +0200

    Legal texts added.

--- src/screens/ImprintScreen.js
+++ src/screens/ImprintScreen.js
@@ -5,7 +5,25 @@ const ImprintScreen = (props) => {
 
     return (
         <View style={styles.container}>
-            <Text>Imprint</Text>
+            <Text>Hochschule Osnabrück</Text>
+            <Text>Albrechtstraße 30</Text>
+            <Text>49076 Osnabrück</Text>
+            <Text>Deutschland</Text>
+            <Text></Text>
+            <Text>Tel: +49 541 969-0</Text>
+            <Text>Fax: +49 541 969-2066</Text>
+            <Text></Text>
+            <Text>Die Hochschule ist eine Körperschaft öffetnlichen Rechts in der Trägerschaft einer Stiftung des öffentlichen Rechts. Sie wird durch den Präsidenten Prof. Dr. Andreas Bertram gesetzlich vertreten.</Text>
+            <Text></Text>
+            <Text>Zuständige Aufsichtsbehörde ist gem. §§59 Abs. 1, 60. Abs. 2 NHG der Stiftungsrat der Stiftung Hochschule Osnabrück, Albrechtstraße 30 49076 Osnabrück.</Text>
+            <Text></Text>
+            <Text>Umsatzsteuer-ID: DE 812 619 579</Text>
+            <Text></Text>
+            <Text>Redaktion - verantowrtlich nach § 55 (2) RStV: </Text>
+            <Text></Text>
+            <Text>Ralf Garten</Text>
+            <Text>Tel: +49 541 969-2177</Text>
+            <Text>webmaster@hs-osnabrueck.de</Text>
         </View>
     )
 }
--- src/screens/PrivacyScreen.js
+++ src/screens/PrivacyScreen.js
@@ -5,7 +5,10 @@ const PrivacyScreen = (props) => {
 
     return (
         <View style={styles.container}>
-            <Text>Privacy</Text>
+            <Text style={styles.subHeading}>Letzte Änderung: 08. November 2019</Text>
+            <Text style={styles.text}>Im Folgenden erläutern wir, welche Informatione während der Nutztun der Applikation erfasst und verarbeitet werden.</Text>
+            <Text style={styles.subHeading}>Personenenbezogenen Daten</Text>
+            <Text style={styles.text}>Diese Applikation sammelt keine personenbezogenen Daten. Eingaben des Nutzers werden nur lokal auf dem Gerät zur benutzerfreundlichen Bedienung gespeochernt, damit die Werte beim erneuten Appstart in die Felder eingesetzt werden können. Zur Berechnung der Empfehlungen werden die eingebebenen Daten einmalig anonym an den Server gesendet. Diese werden über die Berechnung hinaus nicht auf dem Server gespeichert.</Text>
         </View>
     )
 }
@@ -14,6 +17,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+    },
+    text:{
+        fontSize: 16
+    },
+    subHeading:{
+        fontSize: 16,
+        marginVertical: 8,
+        fontWeight: 'bold'
     }
 })
 
