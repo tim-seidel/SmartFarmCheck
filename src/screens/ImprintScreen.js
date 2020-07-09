5a6b05bd43143b85fb5b8d87f2f18b78e0ea345f
Tim Seidel <timseidel@83ad3357.funky.hs-osnabrueck.de>
2020-07-09 14:35:53 +0200

    Adjusted margins in the information screens.

--- src/screens/ImprintScreen.js
+++ src/screens/ImprintScreen.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { ContentText } from '../components/Text'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const ImprintScreen = (props) => {
 
     return (
         <View style={styles.container}>
-            <ContentText>Hochschule Osnabrück</ContentText>
-            <ContentText>Albrechtstraße 30</ContentText>
-            <ContentText>49076 Osnabrück</ContentText>
-            <ContentText>Deutschland</ContentText>
-            <ContentText></ContentText>
-            <ContentText>Tel: +49 541 969-0</ContentText>
-            <ContentText>Fax: +49 541 969-2066</ContentText>
-            <ContentText></ContentText>
-            <ContentText>Die Hochschule ist eine Körperschaft öffetnlichen Rechts in der Trägerschaft einer Stiftung des öffentlichen Rechts. Sie wird durch den Präsidenten Prof. Dr. Andreas Bertram gesetzlich vertreten.</ContentText>
-            <ContentText></ContentText>
-            <ContentText>Zuständige Aufsichtsbehörde ist gem. §§59 Abs. 1, 60. Abs. 2 NHG der Stiftungsrat der Stiftung Hochschule Osnabrück, Albrechtstraße 30 49076 Osnabrück.</ContentText>
-            <ContentText></ContentText>
-            <ContentText>Umsatzsteuer-ID: DE 812 619 579</ContentText>
-            <ContentText></ContentText>
-            <ContentText>Redaktion - verantowrtlich nach § 55 (2) RStV: </ContentText>
-            <ContentText></ContentText>
-            <ContentText>Ralf Garten</ContentText>
-            <ContentText>Tel: +49 541 969-2177</ContentText>
-            <ContentText>webmaster@hs-osnabrueck.de</ContentText>
+            <ScrollView>
+                <ContentText>Hochschule Osnabrück</ContentText>
+                <ContentText>Albrechtstraße 30</ContentText>
+                <ContentText>49076 Osnabrück</ContentText>
+                <ContentText>Deutschland</ContentText>
+                <ContentText></ContentText>
+                <ContentText>Tel: +49 541 969-0</ContentText>
+                <ContentText>Fax: +49 541 969-2066</ContentText>
+                <ContentText></ContentText>
+                <ContentText>Die Hochschule ist eine Körperschaft öffetnlichen Rechts in der Trägerschaft einer Stiftung des öffentlichen Rechts. Sie wird durch den Präsidenten Prof. Dr. Andreas Bertram gesetzlich vertreten.</ContentText>
+                <ContentText></ContentText>
+                <ContentText>Zuständige Aufsichtsbehörde ist gem. §§59 Abs. 1, 60. Abs. 2 NHG der Stiftungsrat der Stiftung Hochschule Osnabrück, Albrechtstraße 30 49076 Osnabrück.</ContentText>
+                <ContentText></ContentText>
+                <ContentText>Umsatzsteuer-ID: DE 812 619 579</ContentText>
+                <ContentText></ContentText>
+                <ContentText>Redaktion - verantowrtlich nach § 55 (2) RStV: </ContentText>
+                <ContentText></ContentText>
+                <ContentText>Ralf Garten</ContentText>
+                <ContentText>Tel: +49 541 969-2177</ContentText>
+                <ContentText>webmaster@hs-osnabrueck.de</ContentText>
+            </ScrollView>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        marginHorizontal: 8
     }
 })
 
--- src/screens/PrivacyScreen.js
+++ src/screens/PrivacyScreen.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { ContentText } from '../components/Text'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const PrivacyScreen = (props) => {
 
     return (
         <View style={styles.container}>
-            <ContentText style={styles.subHeading}>Letzte Änderung: 08. November 2019</ContentText>
-            <ContentText>Im Folgenden erläutern wir, welche Informatione während der Nutztun der Applikation erfasst und verarbeitet werden.</ContentText>
-            <ContentText style={styles.subHeading}>Personenenbezogenen Daten</ContentText>
-            <ContentText>Diese Applikation sammelt keine personenbezogenen Daten. Eingaben des Nutzers werden nur lokal auf dem Gerät zur benutzerfreundlichen Bedienung gespeochernt, damit die Werte beim erneuten Appstart in die Felder eingesetzt werden können. Zur Berechnung der Empfehlungen werden die eingebebenen Daten einmalig anonym an den Server gesendet. Diese werden über die Berechnung hinaus nicht auf dem Server gespeichert.</ContentText>
+            <ScrollView>
+                <ContentText style={styles.subHeading}>Letzte Änderung: 08. November 2019</ContentText>
+                <ContentText>Im Folgenden erläutern wir, welche Informatione während der Nutztun der Applikation erfasst und verarbeitet werden.</ContentText>
+                <ContentText style={styles.subHeading}>Personenenbezogenen Daten</ContentText>
+                <ContentText>Diese Applikation sammelt keine personenbezogenen Daten. Eingaben des Nutzers werden nur lokal auf dem Gerät zur benutzerfreundlichen Bedienung gespeochernt, damit die Werte beim erneuten Appstart in die Felder eingesetzt werden können. Zur Berechnung der Empfehlungen werden die eingebebenen Daten einmalig anonym an den Server gesendet. Diese werden über die Berechnung hinaus nicht auf dem Server gespeichert.</ContentText>
+            </ScrollView>
         </View>
     )
 }
@@ -17,9 +20,10 @@ const PrivacyScreen = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        marginHorizontal: 8
     },
-    subHeading:{
-        marginVertical: 16,
+    subHeading: {
+        marginVertical: 12,
         fontWeight: 'bold'
     }
 })
