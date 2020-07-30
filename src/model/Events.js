7f9e765406993cbf9e62f980a7dcaaf548ff5a94
Tim Seidel <timseidel@83ad3357.funky.hs-osnabrueck.de>
2020-07-30 12:11:44 +0200

    Added a second mocked event

--- src/model/Events.js
+++ src/model/Events.js
@@ -23,25 +23,27 @@ const events = [
     },
     {
         id: "2",
-        startDate: "2020-06-04T16:00:00",
-        endDate: "2020-06-04T18:00:00",
-        title: "Die Fabrik der Zukunft - Vision und Praxis",
-        short: "Künstliche Intelligenz zur Nutzung des Wissens aus digitalen Daten",
-        descripion: `"<h2>Künstliche Intelligenz zur Nutzung des Wissens aus digitalen Daten</h2>
-        <p>Der digitale Fortschritt verändert unser Leben und unsere Arbeitswelt radikal. Zugleich gibt es zunehmen mehr digitale sowie analoge Daten. Eine besondere Herausforderung besteht darin, das in diesen Daten steckende Wissen zu nutzen und daraus einen Mehrwert zu generieren. Künstliche Intelligenz (kurz KI) bietet hierauf Lösungsansätze.<br />
-        Bei der Kooperationsveranstaltung des Mittelstand 4.0-Kompetenzzentrums Lingen (Standort Osnabrück) und des Vereins Deutscher Ingenieure e.V. (VDI) Bezirksverein Osnabrück-Emsland stellen  Experten sowie Gastreferenten Möglichkeiten vor, wie kleine und mittlere Unternehmen KI heute und in der Zukunft gewinnbringend einsetzen können. Umsetzung, Aufwand und Nutzen stehen zur Diskussion und werden anhand von Praxisbeispielen erläutert.</p>
-        <p>Die Online-Veranstaltung findet am Donnerstag, den 04. Juni 2020 statt und beginnt um 16:00 Uhr. Ab 15:45 Uhr wird ein Beitritt in den virtuellen Veranstaltungsraum möglich sein.</p>
-        <p>Veranstaltungsablauf:</p>
+        startDate: "2020-08-12T14:00:00",
+        endDate: "2020-08-12T17:00:00",
+        title: "Chatbots erfolgreich konzipieren und umsetzen",
+        short: 'Im Workshop "Chatbots erfolgreich konzipieren und umsetzen" erkunden Teilnehmenden, die verschiedenen Funktionen und Einsatzbereiche von Chatbots. Dabei steht das eigene Unternehmen im Vordergrund.',
+        descripion: `"<p>Prozessoptimierung, Marketing oder auch Kundenservice sind wesentliche Bestandteile jedes Unternehmens und essenziell für die Kundenbindung. Bei den Anwendungsmöglichkeiten von Künstlicher Intelligenz im Bereich der menschenähnlichen Kommunikation haben sich insbesondere Chatbots von einem Nischenthema zu einer vieldiskutierten sowie häufig verwendeten Technologie entwickelt. <br />
+        Neben klassischen Aufgaben im Service, wie der Beantwortung von einfachen Kundenanfragen, können Chatbots zunehmend auch organisatorische und vertriebliche Aufgaben übernehmen und sogar zur Weiterbildung von Mitarbeitenden eingesetzt werden.<br />
+        Auch kleine und mittlere Unternehmen können die Potenziale von Chatbots in unterschiedlichen Einsatzszenarien nutzen. Fragen die sich dabei ergeben kann lautet: &#8222;Welche Funktionen sind für mein Unternehmen spannend und attraktiv?&#8220; Im Workshop &#8222;Chatbots erfolgreich konzipieren und umsetzen&#8220; erkunden die Teilnehmenden, gemeinsam mit den KI-Trainern des Mittelstand 4.0-Kompetenzzentrums Lingen und Ulf Loetschert, einem Chatbot-Experten der LoyJoy GmbH aus Münster, die verschiedenen Funktionen und Einsatzbereiche von Chatbots. Dabei steht die eigene Unternehmensrealität im Vordergrund. Im Workshop wird so direkt ein individueller Chatbot konzeptioniert und kann getestet werden.</p>
+        <p>Die Inhalte im Überblick:</p>
         <ul>
-        <li>Begrüßung und Einstimmung auf das Thema | Prof. Dr.-Ing. Clemens Westerkamp (Hochschule Osnabrück) &amp; Dr.-Ing. Alfred J.H. Schoo (VDI BV Osnabrück-Emsland e.V.)</li>
-        <li>Künstliche Intelligenz – eine Einordnung | Prof. Dr. rer. nat. Theodor Gervens (Hochschule Osnabrück)</li>
-        <li>Angewandte Forschung zur Prozess- und Energiebedarfsoptimierung | Prof. Dr.-Ing. Hans-Jürgen Pfisterer (Kompetenzzentrum Elektronik &amp; Antriebstechnik (KEA), Hochschule Osnabrück)</li>
-        <li>Predictive Maintenance (Vorausschauende Wartung) | Dipl.-Ing. FH Thorsten Beling (Abacus Maschinenbau GmbH)</li>
-        <li>Demonstration von praktischen Beispielen | Hayyan Alkhatib &amp; Maximilian Haverkamp (Hochschule Osnabrück)</li>
-        <li>Treffen in virtuellen Themen- und Gruppenräumen mit moderierter Diskussion und Zusammenfassung</li>
-        <li>Evaluation </li>
-        </ul>"`,
-        url: 'https://kompetenzzentrum-lingen.digital/termin/die-fabrik-der-zukunft-vision-und-praxis'
+        <li>Vorstellungsrunde</li>
+        <li>Kurzvortrag: &#8222;Chatbots und Künstliche Intellligenz&#8220;</li>
+        <li>Einsatzbereiche und Potentiale von Chatbots</li>
+        <li>Individuelle Konzeption eines Chatbots</li>
+        <li>Nutzung und Test eines Chatbots</li>
+        <li>Abschluss und Evaluation</li>
+        </ul>
+        <p>&nbsp;</p>
+        <p>Um eine intensive Arbeitsphase gewährleisten zu können ist die Teilnehmeranzahl begrenzt. Die Teilnahme ist kostenlos.</p>
+        <p>&nbsp;</p>
+        <p>Eine Anmeldung pro Unternehmen reicht aus.</p>"`,
+        url: 'https://kompetenzzentrum-lingen.digital/termin/chatbots-erfolgreich-konzipieren-und-umsetzen'
     },
 ]
 
