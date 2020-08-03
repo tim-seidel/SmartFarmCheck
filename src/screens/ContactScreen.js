388fe33336269868d35a9a49da43f9cbf996272a
Tim Seidel <timseidel@83ad3357.funky.hs-osnabrueck.de>
2020-08-03 15:37:10 +0200

    ContactScreen Layout changes depending on device rotation

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, StyleSheet, Linking, Platform, } from 'react-native'
+import React, {useState, useEffect} from 'react';
+import { View, StyleSheet, Linking, Platform, Dimensions} from 'react-native'
 
 import InformationCard, { InformationHighlight, InformationText } from '../components/InformationCard';
 import Strings from '../constants/Strings';
@@ -12,7 +12,23 @@ const ActionButton = (props) => {
     </View>)
 }
 
+const isPortrait = () => {
+    const dim = Dimensions.get('screen');
+    return dim.height >= dim.width;
+};
+
 const ContactScreen = (props) => {
+    const [orientation, setOrientation] = useState(isPortrait() ? 'portrait' : 'landscape')
+
+    useEffect(() => {
+        const callback = () => setOrientation(isPortrait() ? 'portrait' : 'landscape');
+
+        Dimensions.addEventListener('change', callback);
+
+        return () => {
+            Dimensions.removeEventListener('change', callback);
+        };
+    }, []);
 
     const contactMailHandler = () => {
         Linking.openURL("mailto:mittelstand40@hs-osnabrueck.de")
@@ -30,38 +46,52 @@ const ContactScreen = (props) => {
         console.log('Über diese App!')
         props.navigation.navigate('About')
     }
-    const contentAboutApp = <ActionButton icon="information-outline" text="Über diese App" onPress={aboutAppHandler}/>
-    const contentMail = <ActionButton icon="email-outline" text="Jetzt E-Mail verfassen" onPress={contactMailHandler}/>
-    const contentPhone = <ActionButton icon="phone" text="Jetzt anrufen" onPress={contactPhoneHandler}/>
-    const contentFeedback = <ActionButton icon="file-document-outline" text="Zum Feedback" onPress={contactFeedbackHandler}/>
-     return (
-        <View style={styles.container}>
-            <ScrollView >
-                <InformationCard title="Über diese App" style={styles.welcomeCard} contentView={contentAboutApp}>
+    const contentAboutApp = <ActionButton icon="information-outline" text="Über diese App" onPress={aboutAppHandler} />
+    const contentMail = <ActionButton icon="email-outline" text="Jetzt E-Mail verfassen" onPress={contactMailHandler} />
+    const contentPhone = <ActionButton icon="phone" text="Jetzt anrufen" onPress={contactPhoneHandler} />
+    const contentFeedback = <ActionButton icon="file-document-outline" text="Zum Feedback" onPress={contactFeedbackHandler} />
+    const cardStyle = orientation === 'portrait' ? styles.contactCardSingle : styles.contactCardGrid
+    return (
+
+        <ScrollView style={styles.scrollView}>
+            <View style={styles.container}>
+                <InformationCard title="Über diese App" style={cardStyle} contentView={contentAboutApp}>
                     <InformationText>{"Informationen über Förderer, Impressum und Datenschutz.\n"}</InformationText>
                 </InformationCard>
-                <InformationCard title="Per Mail" style={styles.welcomeCard} contentView={contentMail}>
+                <InformationCard title="Per Mail" style={cardStyle} contentView={contentMail}>
                     <InformationText>{"Bei allgemeinen Fragen können Sie uns jederzeit per Mail erreichen. Wir werden uns dann entsprechend bei Ihnen zurückmelden.\n\n"}</InformationText>
                     <InformationHighlight>{Strings.mail + "\n"}</InformationHighlight>
                 </InformationCard>
-                <InformationCard title="Telefonisch" style={styles.welcomeCard} contentView={contentPhone}>
+                <InformationCard title="Telefonisch" style={cardStyle} contentView={contentPhone}>
                     <InformationText>{"Natürlich können Sie uns auch telefonisch zu den normalen Bürozeiten erreichen.\n\n"}</InformationText>
                     <InformationHighlight>{"Andreas Ansprechpartner\nTel: 0541-969-1234\n"}</InformationHighlight>
                 </InformationCard>
-                <InformationCard title="App-Feedback" style={styles.welcomeCard} contentView={contentFeedback}>
+                <InformationCard title="App-Feedback" style={cardStyle} contentView={contentFeedback}>
                     <InformationText>{"Bei Fragen zur App oder Problemen können Sie auch hier ein Formular ausfüllen.\n"}</InformationText>
                 </InformationCard>
-            </ScrollView>
-        </View>
+            </View>
+        </ScrollView>
+
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
         flex: 1,
+        justifyContent: 'space-around'
     },
-    welcomeCard: {
+    scrollView: {
         margin: 8
+    },
+    contactCardSingle: {
+        marginVertical: 8,
+        width: '90%'
+    },
+    contactCardGrid: {
+        marginVertical: 8,
+        width: '49%'
     },
     action: {
         marginHorizontal: 8
