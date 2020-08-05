14298b09cd6204053a87d727dcb1ca34015ff9bc
Tim Seidel <timseidel@83ad3357.funky.hs-osnabrueck.de>
2020-08-05 14:00:29 +0200

    Transformed EvaluationScreen to functional compoment

--- src/screens/EvaluationScreen.js
+++ src/screens/EvaluationScreen.js
@@ -1,36 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import { useNavigation, useRoute } from '@react-navigation/native';
-
 import NoContentView from '../components/NoContentView';
 import EvaluationListItemView from '../components/EvaluationListItemView';
 import InformationCard, { InformationHighlight, InformationText } from '../components/InformationCard';
 import Strings from '../constants/Strings';
 import { HeadingText } from '../components/Text';
+import { useStateValue } from '../StateProvider';
 
-class EvaluationScreen extends React.Component {
+const EvaluationScreen = (props) => {
+    const [{colorTheme}] = useStateValue()
+    const [evalulationState, setEvaluationState] = useState({ error: null, isLoaded: false, evaluation: [] })
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            evaluation: []
+    useEffect(() => {
+        if (!evalulationState.isLoaded) {
+            evaluate();
         }
-    }
+    }, [evalulationState.isLoaded])
 
-    componentDidMount() {
-        if (!this.state.isLoaded) {
-            this.evaluate();
-        }
-    }
-
-    evaluate() {
-        const data = this.props.route.params;
+    function evaluate() {
+        const data = props.route.params;
         console.log("Sending input", data)
 
-        if (!this.state.isLoaded) {
+        if (!evalulationState.isLoaded) {
             fetch('https://pas.coala.digital/v1/evaluate', {
                 method: 'POST',
                 headers: {
@@ -42,79 +34,67 @@ class EvaluationScreen extends React.Component {
                 .then(response => response.json())
                 .then(json => {
                     console.log("Received evaluation", json)
-                    this.setState({
-                        isLoaded: true,
-                        evaluation: json,
-                        error: null
-                    })
+                    setEvaluationState({ isLoaded: true, evaluation: json, error: null })
                 })
                 .catch(error => {
                     console.log("Error", error)
-                    this.setState({
-                        isLoaded: false,
-                        error: error
-                    })
+                    setEvaluationState.setState({ isLoaded: false, error: error, evaluation: [] })
                 })
         }
     }
 
-    onRetryHandler() {
-        this.setState({
-            isLoaded: false,
-            error: null
-        }, this.evaluate.bind(this))
+    function onRetryHandler() {
+        setEvaluationState({ isLoaded: false, error: null, evaluation: [] })
     }
 
-    measureSelectedHandler(uuid) {
-        this.props.navigation.navigate("EvaluationDetail", uuid)
+    function measureSelectedHandler(uuid) {
+        props.navigation.navigate("EvaluationDetail", uuid)
     }
 
-    render() {
-        const { error, isLoaded, evaluation } = this.state;
+    const { error, isLoaded, evaluation } = evalulationState
 
-        if (error) {
-            return <View style={styles.container}><NoContentView icon="emoticon-sad-outline" onRetry={this.onRetryHandler.bind(this)} title={Strings.evaluation_loading_error}></NoContentView></View>
-        } else if (!isLoaded) {
-            return <View style={styles.container}><NoContentView icon="cloud-download" loading title={Strings.evaluation_loading}></NoContentView></View>
-        } else if (!evaluation || evaluation.length === 0) {
-            return <View style={styles.container}><NoContentView icon="emoticon-sad-outline" onRetry={this.onRetryHandler.bind(this)} title={Strings.evaluation_loading_empty}></NoContentView></View>
-        } else {
-            let max = 0;
-            evaluation.forEach(e => {
-                if (e.rating > max) {
-                    max = e.rating
-                }
-            })
-            return (
-                <View style={styles.container} >
-                    <FlatList
-                        style={styles.list}
-                        data={evaluation}
-                        ListHeaderComponent={
-                            <View>
-                                <InformationCard>
-                                    <InformationText>Hier sehen Sie die auf Basis Ihrer Antworten </InformationText>
-                                    <InformationHighlight style={styles.explanationHighlight}>gewichteten Maßnahmen</InformationHighlight>
-                                    <InformationText>. Möchten Sie sich über eine dieser Maßnahme informieren, so </InformationText>
-                                    <InformationHighlight>tippen</InformationHighlight>
-                                    <InformationText> Sie diese einfach an.</InformationText>
-                                </InformationCard>
-                                <HeadingText large weight="bold" style={styles.heading}>Ergebnisse:</HeadingText>
-                            </View>}
-                        renderItem={({ item }) => (
-                            <EvaluationListItemView
-                                key={item.uuid}
-                                rating={Math.round((item.rating / max) * 100)}
-                                title={item.name}
-                                short={item.excerpt}
-                                measureSelected={() => this.measureSelectedHandler(item.uuid)}
-                            />
-                        )}
-                        keyExtractor={item => item.uuid}
-                    />
-                </View>
-            );
-        }
+    if (error) {
+        return <View style={{...styles.container, backgroundColor: colorTheme.background}}><NoContentView icon="emoticon-sad-outline" onRetry={onRetryHandler} title={Strings.evaluation_loading_error}></NoContentView></View>
+    } else if (!isLoaded) {
+        return <View style={{...styles.container, backgroundColor: colorTheme.background}}><NoContentView icon="cloud-download" loading title={Strings.evaluation_loading}></NoContentView></View>
+    } else if (!evaluation || evaluation.length === 0) {
+        return <View style={{...styles.container, backgroundColor: colorTheme.background}}><NoContentView icon="emoticon-sad-outline" onRetry={onRetryHandler} title={Strings.evaluation_loading_empty}></NoContentView></View>
+    } else {
+        let max = 0;
+        evaluation.forEach(e => {
+            if (e.rating > max) {
+                max = e.rating
+            }
+        })
+        return (
+            <View style={{...styles.container, backgroundColor: colorTheme.background}} >
+                <FlatList
+                    style={styles.list}
+                    data={evaluation}
+                    ListHeaderComponent={
+                        <View>
+                            <InformationCard>
+                                <InformationText>Hier sehen Sie die auf Basis Ihrer Antworten </InformationText>
+                                <InformationHighlight style={styles.explanationHighlight}>gewichteten Maßnahmen</InformationHighlight>
+                                <InformationText>. Möchten Sie sich über eine dieser Maßnahme informieren, so </InformationText>
+                                <InformationHighlight>tippen</InformationHighlight>
+                                <InformationText> Sie diese einfach an.</InformationText>
+                            </InformationCard>
+                            <HeadingText large weight="bold" style={styles.heading}>Ergebnisse:</HeadingText>
+                        </View>}
+                    renderItem={({ item }) => (
+                        <EvaluationListItemView
+                            key={item.uuid}
+                            rating={Math.round((item.rating / max) * 100)}
+                            title={item.name}
+                            short={item.excerpt}
+                            measureSelected={() => measureSelectedHandler(item.uuid)}
+                        />
+                    )}
+                    keyExtractor={item => item.uuid}
+                />
+            </View>
+        );
     }
 }
 
@@ -131,10 +111,4 @@ const styles = StyleSheet.create({
     }
 });
 
-// Wrap for navigation
-export default function (props) {
-    const navigation = useNavigation();
-    const route = useRoute();
-
-    return <EvaluationScreen {...props} route={route} navigation={navigation} />;
-}
+export default EvaluationScreen
