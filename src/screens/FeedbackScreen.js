c98bafc588abd7fdec87ac408fe09c5fe75cd97b
Tim Seidel <timseidel@83ad3357.funky.hs-osnabrueck.de>
2020-08-05 15:29:55 +0200

    Added a FeedbackScreen in progress message

--- src/screens/FeedbackScreen.js
+++ src/screens/FeedbackScreen.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { useStateValue } from '../StateProvider'
+import InformationCard, { InformationText } from '../components/InformationCard'
 
 const FeedbackScreen = (props) => {
     const [{colorTheme}] = useStateValue()
 
     return (
         <View style={{...styles.container, backgroundColor: colorTheme.background}}>
+            <InformationCard style={styles.card} title="Work in progress">
+                <InformationText>An der Möglichkeit zum spezifischen Feedback zu Problemen und Aktionen in der App wird gerade gearbeitet.</InformationText>
+            </InformationCard>
         </View>
     )
 }
@@ -14,6 +18,9 @@ const FeedbackScreen = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    card:{
+        margin: 8
     }
 })
 
