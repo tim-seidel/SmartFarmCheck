b2c31c4125113e6cc67aab08da10ce3a0af52ea3
Tim Seidel <timseidel@Tims-MBP.lan>
2020-08-27 12:41:11 +0200

    Added missing properties

--- src/models/Question.js
+++ src/models/Question.js
@@ -1,9 +1,12 @@
 export default class Question {
-    constructor(uuid, text, descripton, validator, formPriority){
+    constructor(uuid, text, description, validator, formPriority){
         this.uuid = uuid
         this.text = text
-        this.descripton = descripton
+        this.description = description
         this.validator = validator 
         this.formPriority = formPriority 
+        this.input = ''
+        this.validity = 'unedited'
     }
 }
+ 
