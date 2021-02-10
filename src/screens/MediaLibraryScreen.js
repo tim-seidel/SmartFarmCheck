603b5de6f8603c22a3c421ff485c03369ff170d8
Tim Seidel <account@timseidel.de>
2021-02-10 11:10:39 +0100

    Imporved the vertical layout for the media library

--- src/screens/MediaLibraryScreen.js
+++ src/screens/MediaLibraryScreen.js
@@ -1,7 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
+import { View, StyleSheet, FlatList, Platform, Dimensions } from 'react-native'
 import NetInfo from '@react-native-community/netinfo'
 import { useDispatch, useSelector } from 'react-redux'
+import * as Device from 'expo-device'
 
 import RootView from '../components/RootView'
 import { VIDEOSCREEN } from '../constants/Paths'
@@ -13,13 +14,37 @@ import InformationCard, { InformationText } from '../components/InformationCard'
 import { HeadingText } from '../components/Text'
 import Keys from '../constants/Keys'
 
+const isPortrait = () => {
+    const dim = Dimensions.get('screen');
+    return dim.height >= dim.width;
+};
+
 const MediaLibraryScreen = (props) => {
-    const dispatch = useDispatch()
-    const mediaLibrary = useSelector(state => state.mediaLibrary.all)
+    const [orientation, setOrientation] = useState(isPortrait() ? 'portrait' : 'landscape')
+    const [isTablet, setIsTablet] = useState(Platform.isPad)
 
     const [isLoading, setIsLoading] = useState(false)
     const [hasNoNetwork, setHasNoNetwork] = useState(false)
     const [errorCode, setErrorCode] = useState(0)
+
+    const dispatch = useDispatch()
+    const mediaLibrary = useSelector(state => state.mediaLibrary.all)
+
+    useEffect(() => {
+        const callback = ({ screen }) => {
+          setOrientation(screen.height >= screen.width ? 'portrait' : 'landscape')
+        }
+        const checkTablet = async () => {
+          const type = Device.getDeviceTypeAsync()
+          setIsTablet(!(type === Device.DeviceType.PHONE || type === Device.DeviceType.UNKNOWN))
+        }
+        checkTablet()
+    
+        Dimensions.addEventListener('change', callback);
+        return () => {
+          Dimensions.removeEventListener('change', callback);
+        };
+    }, []);    
 
     useEffect(() => {
         checkAndLoadVideoList()
@@ -64,6 +89,8 @@ const MediaLibraryScreen = (props) => {
     } else {
         contentView =
             <FlatList
+                key={(isTablet && orientation === 'landscape' ? 'l' : 'p')} //Need to change the key aswell, because an on the fly update of numColumns is not supported and a full rerender is necessary
+                numColumns={isTablet && orientation === 'landscape' ? 2 : 1}
                 style={styles.list}
                 data={mediaLibrary}
                 ListHeaderComponent={
@@ -73,13 +100,14 @@ const MediaLibraryScreen = (props) => {
                             toggleStoreKey={Keys.INFORMATION_TOGGLE_MEDIALIBRARY_SCREEN}
                             title={Strings.medialibrary_card_title}
                             style={styles.card}
-                        >
+                            >
                             <InformationText>{Strings.medialibrary_card_description}</InformationText>
                         </InformationCard>
                         <HeadingText large weight="bold" style={styles.heading}>{Strings.medialibrary_heading}</HeadingText>
                     </View>}
                 renderItem={({ item }) => (
                     <MediaLibraryListViewItem
+                        style={styles.mediaLibraryColumn}
                         title={item.title}
                         description={item.description}
                         onShowVideo={() => showVideoHandler(item.url)}
@@ -97,16 +125,20 @@ const MediaLibraryScreen = (props) => {
 
 const styles = StyleSheet.create({
     list: {
-        marginHorizontal: 8,
-        marginVertical: 4
+        margin: 4
     },
     heading: {
         marginTop: 16,
         marginBottom: 8,
-        marginStart: 2
+        marginStart: 6
     },
     card: {
-        marginTop: 4
+        marginTop: 4,
+        marginHorizontal: 4
+    },
+    mediaLibraryColumn: {
+        margin: 4,
+        flex: 1
     }
 })
 
