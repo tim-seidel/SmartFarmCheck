044ff2c293d45820323d23baaa478889012b0387
Tim Seidel <account@timseidel.de>
2021-06-24 14:13:11 +0200

    Improved event items

--- src/components/EventListViewItem.js
+++ src/components/EventListViewItem.js
@@ -7,9 +7,12 @@ import moment from 'moment'
 import View from './common/View'
 import IconButton from './common/IconButton'
 import { HeadingText, ContentText } from './common/Text'
+import Separator from './common/Separator'
 
 import Layout from '../constants/Layout'
 import { darkTheme, lightTheme } from '../constants/Colors'
+import Strings from '../constants/Strings'
+import API from '../constants/API'
 
 const EventListViewItem = (props) => {
     const event = props.event
@@ -18,32 +21,50 @@ const EventListViewItem = (props) => {
     return (
         <View component style={{ ...styles.event, ...props.style }}>
             <HeadingText weight="bold">{event.title}</HeadingText>
-            <View style={styles.separator} />
-
-            {event.image && <Image style={styles.image} source={{ uri: `https://pas.sei-farbenfroh.de/v1/files/${event.image}` }} />}
-
+            <Separator style={styles.separator} />
+            {event.image && <Image style={styles.image} source={{ uri: `${API.URL}/${API.VERSION}/files/${event.image}` }} />}
             <View style={styles.row}>
-                <Icon style={styles.leftIcon} color={colorTheme.textPrimary} name="calendar-outline" size={24}></Icon>
+                <Icon
+                    style={styles.leftIcon}
+                    color={colorTheme.textPrimary}
+                    name="calendar-outline"
+                    size={24} />
                 <ContentText style={styles.text} large>{formatDate(moment(event.startDate), moment(event.endDate))}</ContentText>
             </View>
             <View style={styles.row}>
-                <Icon style={styles.leftIcon} name="information-outline" color={colorTheme.textPrimary} size={24}></Icon>
-                <ContentText style={styles.text} light numberOfLines={3}>{event.description}</ContentText>
+                <Icon
+                    style={styles.leftIcon}
+                    name="information-outline"
+                    color={colorTheme.textPrimary}
+                    size={24} />
+                <ContentText style={styles.text} light numberOfLines={3}>{event.description ?? Strings.event_no_description_available}</ContentText>
             </View>
             {event.maxParticipantCount > 0 &&
                 <View style={styles.row}>
                     <Icon style={styles.leftIcon} color={colorTheme.textPrimary} name="account-multiple" size={24} />
-                    <ContentText style={styles.text}>{"Diese Veranstaltung ist auf " + event.maxParticipantCount + " Teilnehmer begrenzt!"}</ContentText>
+                    <ContentText style={styles.text}>{Strings.event_max_participant_1 + event.maxParticipantCount + Strings.event_max_participant_2}</ContentText>
                 </View>}
             <View style={styles.eventButtonRow}>
                 <View style={styles.eventButtonWrapper}>
-                    <IconButton icon="web" text="Details" fontSize={15} onPress={props.onDetailPress} />
+                    <IconButton
+                        icon="web"
+                        fontSize={15}
+                        text={Strings.event_details}
+                        onPress={props.onDetailPress} />
                 </View>
                 <View style={styles.eventButtonMarginWrapper}>
-                    <IconButton icon="account-plus-outline" text="Anmelden" fontSize={15} onPress={props.onRegisterPress} />
+                    <IconButton
+                        icon="account-plus-outline"
+                        text={Strings.event_register}
+                        fontSize={15}
+                        onPress={props.onRegisterPress} />
                 </View>
                 <View style={styles.eventButtonWrapper}>
-                    <IconButton icon="bookmark-outline" text="Merken" fontSize={15} onPress={props.onExportToCalendarPress} />
+                    <IconButton
+                        icon="bookmark-outline"
+                        text={Strings.event_remember}
+                        fontSize={15}
+                        onPress={props.onExportToCalendarPress} />
                 </View>
             </View>
         </View>
@@ -51,12 +72,18 @@ const EventListViewItem = (props) => {
 }
 
 function formatDate(start, end) {
+    const date0 = moment(0)
+    const hasStart = !date0.isSame(start)
+    const hasEnd = !date0.isSame(end)
+
+    if (!hasStart && !hasEnd) return Strings.event_time_undefined
+
     if (start.isSame(end, 'day')) {
         return start.format("DD.MM.YYYY | HH:mm") + " - " + end.format("HH:mm")
     } else if (start.isSame(end, 'year')) {
         return start.format("DD.MM HH:mm") + " - " + end.format("DD.MM.YYYY HH:mm")
     } else {
-        return start.format("DD.MM.YYYY HH:mm") + " - " + end.format("DD.MM.YYYY HH:mm")
+        return (hasStart ? start.format("DD.MM.YYYY HH:mm") : Strings.event_start_or_end_undefined) + " - " + (hasEnd ? end.format("DD.MM.YYYY HH:mm") : Strings.event_start_or_end_undefined)
     }
 }
 
@@ -69,8 +96,6 @@ const styles = StyleSheet.create({
         borderRadius: Layout.borderRadius
     },
     separator: {
-        backgroundColor: Layout.borderColor,
-        height: Layout.borderWidth,
         marginVertical: 8
     },
     text: {
