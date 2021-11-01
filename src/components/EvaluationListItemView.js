1b7ce3e66648173600f0e5729b084cb7c0169690
Tim Seidel <account@timseidel.de>
2021-11-01 11:26:40 +0100

    Adjusted rating layout

--- src/components/EvaluationListItemView.js
+++ src/components/EvaluationListItemView.js
@@ -22,7 +22,7 @@ function EvaluationListItemView(props) {
                     <Text style={{...styles.rating, color: props.rating >= good_threshold ? 'green' : 'orange'}}>{zeroPad(props.rating, 2)}%</Text>
                     <View style={{ ...styles.divider, backgroundColor: colorTheme.textPrimary }} />
                     <View style={styles.measureContent}>
-                        <HeadingText large>{props.title}</HeadingText>
+                        <HeadingText large weight="bold">{props.title}</HeadingText>
                         <ContentText light numberOfLines={3} style={{ marginVertical: 4 }}>{props.short}</ContentText>
                     </View>
                     <Icon style={{ ...styles.detailIcon, color: colorTheme.textPrimary }} name="chevron-right" size={32} />
@@ -39,7 +39,6 @@ const styles = StyleSheet.create({
         borderColor: Layout.borderColor,
         borderWidth: Layout.borderWidth,
         overflow: "hidden"
-
     },
     innerWrapper: {
         flexDirection: "row",
@@ -48,18 +47,16 @@ const styles = StyleSheet.create({
         paddingVertical: 12
     },
     rating: {
+        width: 80,
+        fontWeight: "bold",
         fontSize: 28,
-        fontWeight: "700",
         textAlign: "center",
-        textAlignVertical: "center",
-        marginEnd: 8,
-        width: 80
+        textAlignVertical: "center"
     },
     divider: {
         width: Layout.borderWidth,
         height: "100%",
-        marginStart: 4,
-        marginEnd: 8
+        marginHorizontal: 8
     },
     measureContent: {
         flexDirection: "column",
